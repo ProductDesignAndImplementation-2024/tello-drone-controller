8599bacde2d1eb198951423f7d9a6ab38407f029
Jesse Mikkola <jesse.mikkola@live.fi>
2024-11-06 13:50:55 +0200

    countour detection, image cropping

--- drone_controller.py
+++ drone_controller.py
@@ -2,37 +2,51 @@ from djitellopy import Tello
 import cv2
 import time
 import numpy as np
+import image_processing as imgp
 
 tello = Tello()
 tello.connect()
 
 tello.set_video_direction(tello.CAMERA_DOWNWARD)
 
-tello.streamon()
-
-def nothing(x):
-    pass
-
 cv2.namedWindow("Tello-drone Info", cv2.WINDOW_NORMAL)
-cv2.namedWindow("Before and After")
-cv2.createTrackbar("Threshold", "Before and After", 170, 255, nothing)
 
 def fetch_tello_info_window_data():
     info_battery = tello.get_battery()
-    info_flighttime = tello.get_flight_time()
+    info_battery_estimate = 780 - (int)((90 - (info_battery - 10)) * 8.66)
+    if info_battery_estimate < 0:
+        info_battery_estimate = 0
 
-    #time.sleep(1)
+    info_flighttime = tello.get_flight_time()
+    info_height = tello.get_height()
+
+    info_speed_x = tello.get_speed_x()
+    info_speed_y = tello.get_speed_y()
+    info_speed_z = tello.get_speed_z()
+
+    info_roll = tello.get_roll()
+    info_pitch = tello.get_pitch()
+
+    info_temp_avg = tello.get_temperature()
+    info_temp_min = tello.get_lowest_temperature()
+    info_temp_max = tello.get_highest_temperature()
 
     return {
         "Battery": f"{info_battery}",
-        "Flight time": f"{info_flighttime}"
+        "Battery Estimate": f"{info_battery_estimate}",
+        "Flight time": f"{info_flighttime}s",
+        "Height": f"{info_height}cm",
+        
+        "Speed": f"{info_speed_x}, {info_speed_y}, {info_speed_z}",
+        "Roll-Pitch": f"{info_roll}, {info_pitch}",
+        "Temp (avg, min, max)": f"{info_temp_avg}C, {info_temp_min}C, {info_temp_max}C"
     }
 
 def display_tello_info_window(info):
-    info_image = np.zeros((300, 400, 3), dtype=np.uint8)
+    info_image = np.zeros((600, 800, 3), dtype=np.uint8)
 
     font = cv2.FONT_HERSHEY_COMPLEX
-    font_scale = 0.6
+    font_scale = 1.0
     color = (255, 255, 255)
     line_type = 2
 
@@ -41,52 +55,80 @@ def display_tello_info_window(info):
         text = f"{key}: {value}"
 
         # battery text color
-        if key == "Battery":
+        if key == "Battery" or key == "Battery Estimate":
             if int(value) < 15:
-                color = (255, 0, 0)
+                color = (0, 0, 255)
             elif int(value) < 30:
-                color = (255, 255, 0)
+                color = (0, 255, 255)
             else:
                 color = (0, 255, 0)
         else:
             color = (255, 255, 255)
                 
         cv2.putText(info_image, text, (10, y_offset), font, font_scale, color, line_type)
-        y_offset += 30
+        y_offset += 45
 
     cv2.imshow("Tello-drone Info", info_image)
 
 def take_picture():
     frame_read = tello.get_frame_read()
-    time.sleep(0.5)
     cv2.imwrite("picture.png", frame_read.frame)
 
-def display_picture():
     original_image = cv2.imread("picture.png", cv2.IMREAD_GRAYSCALE)
-    threshold_value = cv2.getTrackbarPos("Threshold", "Before and After")
+    cropped_image = original_image[0:240, 0:320]
+    cv2.imwrite("picture.png", cropped_image)
 
-    _, processed_image = cv2.threshold(original_image, threshold_value, 255, cv2.THRESH_BINARY_INV)
+# WASD = moveement
+# E-Q = rotate
+# R-F = up-down
+# K = flip forward
+def movement_controls(key):
+    if key & 0xFF == ord('w'):
+        tello.move_forward(20)
+    elif key &0xFF == ord('s'):
+        tello.move_back(20)
+    elif key &0xFF == ord('a'):
+        tello.move_left(20)
+    elif key &0xFF == ord('d'):
+        tello.move_right(20)
+    elif key &0xFF == ord('e'):
+        tello.rotate_clockwise(20)
+    elif key &0xFF == ord('q'):
+        tello.rotate_counter_clockwise(20)
+    elif key &0xFF == ord('r'):
+        tello.move_up(20)
+    elif key &0xFF == ord('f'):
+        tello.move_down(20)
+    elif key &0xFF == ord('k'):
+        tello.flip_forward()
+    else:
+        return False
+    
+    return True
 
-    combined_image = np.hstack((original_image, processed_image))
-    cv2.imshow("Before and After", combined_image)
 
 while True:
     tello_info = fetch_tello_info_window_data()
     display_tello_info_window(tello_info)
 
-    display_picture()
+    processed = imgp.display_picture()
+    imgp.find_landing_pad(processed)
 
     key = cv2.waitKey(1)
-    if key & 0xFF == ord('q'):
+    if key & 0xFF == ord('x'):
         break
     elif key & 0xFF == ord('t'):
+        tello.streamon()
         tello.takeoff()
     elif key & 0xFF == ord('l'):
         tello.land()
+        tello.streamoff()
     elif key & 0xFF == ord('p'):
         take_picture()
+        #processed = imgp.display_picture()
+        #imgp.find_landing_pad(processed)
+    elif movement_controls(key):
+        continue
         
-tello.land()
-
 cv2.destroyAllWindows()
 tello.end()
--- image_processing.py
+++ image_processing.py
@@ -1,35 +1,75 @@
 import cv2
 import numpy as np
 
-# Load the original image in grayscale
-original_image = cv2.imread("data/image.png", cv2.IMREAD_GRAYSCALE)
-
-# Callback function for trackbar (does nothing but needed for OpenCV trackbars)
 def nothing(x):
     pass
 
-# Create a named window
+def save_processed_image():
+    image = display_picture()
+    cv2.imwrite("processed.png", image)
+
 cv2.namedWindow("Before and After")
+cv2.createTrackbar("Threshold", "Before and After", 25, 255, nothing)
 
-# Create a trackbar for adjusting the threshold
-cv2.createTrackbar("Threshold", "Before and After", 170, 255, nothing)
+min_area = 64
+max_area = 10000
 
-while True:
-    # Get the current position of the trackbar
+cv2.namedWindow("Landing Pad Detection")
+#cv2.resizeWindow("Landing Pad Detection", 640, 400)
+cv2.createTrackbar("Min Area", "Landing Pad Detection", min_area, 10000, nothing)
+cv2.createTrackbar("Max Area", "Landing Pad Detection", max_area, 10000, nothing)
+
+
+def display_picture():
+    original_image = cv2.imread("picture.png", cv2.IMREAD_GRAYSCALE)
+    #print(original_image.shape)
+    # remove excess pixels from image
+    #original_image = cv2.resize(original_image, [320, 240])
+
     threshold_value = cv2.getTrackbarPos("Threshold", "Before and After")
 
-    # Apply the threshold with the current trackbar value
     _, processed_image = cv2.threshold(original_image, threshold_value, 255, cv2.THRESH_BINARY_INV)
 
-    # Combine the original and processed images side by side
     combined_image = np.hstack((original_image, processed_image))
-
-    # Display the combined image
     cv2.imshow("Before and After", combined_image)
 
-    # Break the loop if the 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    return processed_image
 
-# Close all OpenCV windows
-cv2.destroyAllWindows()
+processed_image = display_picture()
+
+def find_landing_pad(processed_image):
+    min_area = cv2.getTrackbarPos("Min Area", "Landing Pad Detection")
+    max_area = cv2.getTrackbarPos("Max Area", "Landing Pad Detection")
+
+    processed_image_2 = processed_image.copy()
+    processed_image_2 = cv2.cvtColor(processed_image_2, cv2.COLOR_GRAY2BGR)
+    contours, _ = cv2.findContours(processed_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    landing_pad_contour = None
+
+    for contour in contours:    
+        area = cv2.contourArea(contour)
+        if min_area < area < max_area:
+            landing_pad_contour = contour
+            break
+
+    if landing_pad_contour is not None:
+        x, y, w, h = cv2.boundingRect(landing_pad_contour)
+        rect_color = (0, 0, 255)
+        cv2.rectangle(processed_image_2, (x, y), (x + w, y + h), rect_color, 2)
+    
+    # Show the result
+    cv2.imshow("Landing Pad Detection", processed_image_2)
+
+if __name__ == "__main__":
+    while True:
+        # Exit on pressing 'x'
+
+        key = cv2.waitKey(1)
+        if key &0xFF == ord('x'):
+            break
+        elif key &0xFF == ord('p'):
+            find_landing_pad(processed_image)
+            
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
